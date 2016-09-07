325fd2b8633a67c0f09ab645d92720d481380105
王灏 <752106129@qq.com>
2016-09-07 20:52:44 +0800

    complete 0006

--- 0006/0006.py
+++ 0006/0006.py
@@ -8,21 +8,26 @@ from collections import Counter
 from os import listdir
 import re
 
-
 def DiaryCount(diaryPath):
-    importantWord=list()
+    importantWord=dict()
+    #需要过滤词，应为存在 a the等词的干扰
+    filter_word=['the', 'in', 'of', 'and', 'to', 'has', 'that', 's', 'is', 'are', 'a', 'with', 'as', 'an','it']
     filelist=listdir(diaryPath)
     for diary in filelist:
-        diarywords=list()
         if len(diary.split('.'))>1 and diary.split('.')[1]=="txt":
             with open(diaryPath+'/'+diary) as fr:
-                for line in fr:
-                    line.strip()
-                    lineword=re.findall(r'\w+',line)
-                    diarywords.extend(lineword)
-            c=Counter(diarywords)
+                content=fr.read()
+                content=content.strip()
+                wordlist=re.findall(r'\w+',content)
+#                print wordlist
+            c=Counter(wordlist)
+            #去除过滤词
+            for filword in filter_word:
+#                c[filword]=0
+                del c[filword]
+            #选择最高频词为最重要的词
             diary_Word=c.most_common(1)
-            importantWord.extend(diary_Word)
+            importantWord[diary]=diary_Word[0][0]
     return importantWord
     
 if __name__=="__main__":
