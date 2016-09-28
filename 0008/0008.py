e72d4e40f604d75b14b4ce96004bfdcd443ce6d5
王灏 <752106129@qq.com>
2016-09-28 21:32:35 +0800

    Add newfile.sh

---
+++ 0008/0008.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 27 10:52:44 2016
+
+@author: MagicWang
+"""
+
---
+++ newfile.sh
@@ -0,0 +1,19 @@
+#shell 的空格很重要
+file_number=7
+new_file_path=0000
+flag=9
+while :
+do
+	if [ ! -d "$new_file_path" ]; then
+		mkdir "$new_file_path"
+		cd "$new_file_path"
+		touch readme.md
+		exit 0
+	fi
+	file_number=$[$file_number+1]
+	if [ $file_number -gt $flag ]; then
+		new_file_path="00"$file_number""
+	else
+		new_file_path="000"$file_number""
+	fi
+done
